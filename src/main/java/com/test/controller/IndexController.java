e185c8ee0f98fc00a61ef7aac957b57f4c1f7c72
janghyeonwoo <blue672@naver.com>
2026-03-01 23:40:10 +0900

    init

---
+++ src/main/java/com/test/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.test.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("aaaaaaaaaaaaaaaaa");
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("data", "Hello World!");
+        mv.setViewName("/WEB-INF/view/index.jsp");
+
+        return mv;
+    }
+}
